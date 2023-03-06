2e7f819a5c5b88c2c7bc10fce9b87dba5e2c44cb
ProgrammingComputer1 <>
2023-03-06 17:50:11 -0500

    downloadable w/ updated icons

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,18 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticDevScouting2023 = "dev-scouting-2023-v1";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
   "/js/bootstrap.min.js",
+  "/js/bootstrap.min.js.map",
   "/js/jquery-3.6.3.min.js",
-  "/js/umd.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/js/umd.js"
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevScouting2023).then(cache => {
       cache.addAll(assets);
     })
   );
