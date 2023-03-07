f7697d63bd581bacd248571ec392bea5820e0a97
ProgrammingComputer1 <>
2023-03-06 19:52:14 -0500

    add database.js to includes areas

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,11 +3,12 @@ const assets = [
   "/",
   "/index.html",
   "/css/style.css",
-  "/js/app.js",
   "/js/bootstrap.min.js",
   "/js/bootstrap.min.js.map",
   "/js/jquery-3.6.3.min.js",
-  "/js/umd.js"
+  "/js/umd.js",
+  "/js/app.js",
+  "/js/database.js"
 ];
 
 self.addEventListener("install", installEvent => {
